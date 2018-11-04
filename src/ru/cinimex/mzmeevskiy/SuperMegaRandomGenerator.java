9023b4b72b06993d6743f673cb23db2b7622a2b2
Mikhail Zmeevskiy <xmishon17@gmail.com>
2018-11-04 18:43:08 +0300

    add a comments

--- src/ru/cinimex/mzmeevskiy/SuperMegaRandomGenerator.java
+++ src/ru/cinimex/mzmeevskiy/SuperMegaRandomGenerator.java
@@ -1,27 +1,44 @@
+
 package ru.cinimex.mzmeevskiy;
 
 import java.security.SecureRandom;
 
+/**
+ * SuperMegaRandomGenerator
+ * This class provides two methods for generating strings
+ * Version 1.0.0
+ */
+
 public class SuperMegaRandomGenerator {
 
+    /**
+     * generates string, containing symbols A-Z, a-z and numbers 0-9
+     * @param len - the length of generated string
+     * @return a {@Code String} of length {@Code len}
+     */
     public static String generateCharNumString(int len) {
         SecureRandom rand = new SecureRandom();
         StringBuilder buf = new StringBuilder(len);
         int num;
         for (int i = 0; i < len; i++){
             num = rand.nextInt(62);
-            if(num < 10) {
-                buf.append((char) (num + 48));
-            } else if (num < 36) {
-                buf.append((char) (num + 55));
+            if(num < 10) { // range 0-9
+                buf.append((char) (num + 48)); //48-57 codes is numbers in UTF-16
+            } else if (num < 36) { //range 10-35
+                buf.append((char) (num + 55)); //10+55=65, 65-90 is letters A-Z in UTF-16
             } else {
-                buf.append((char) (num + 61));
+                buf.append((char) (num + 61)); //36+61=97, 97-122 is letter a-z in UTF-16
             }
         }
 
         return buf.toString();
     }
 
+    /**
+     * generates string, containing numbers 0-9
+     * @param len - the length of generated string
+     * @return a {@Code String} of length {@Code len}
+     */
     public static String generateNumericString(int len) {
         SecureRandom rand = new SecureRandom();
         StringBuilder buf = new StringBuilder(len);
